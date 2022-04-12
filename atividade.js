4d6ba7c90126000313a6f3073add147e3be7dd00
Kauê Godêncio <96804509+KaueGodencio@users.noreply.github.com>
2022-04-12 16:46:12 -0300

    Add files via upload

---
+++ atividade.js
@@ -0,0 +1,46 @@
+let idade = 23;
+
+if (idade >= 18 ) {
+    console.log ("ACESSO PERMITIDO."); 
+ 
+    } else { 
+    console.log ("ACESSO NÃO PERMITIDO PARA MENORES DE 18 ANOS.")
+    console.log ("FIM"); }
+
+
+let dataEvento = 12/04/2022;
+let dataAtual = 08/04/2022;
+
+        if(dataEvento > dataAtual) {
+            console.log ("DATA PERMITIDA.") ;             } 
+           
+            else{
+                console.log("DATA NÃO PERMITIDA.")
+                console.log("FIM")
+
+            }     
+            
+let nomeParticipantes = ['Kaue silva','Isabela santos', 'Eduardo Oliveira', 'Julia Godencio','Lourdes de Jesus']
+let quantidade = 100;
+
+        if(nomeParticipantes.length <= quantidade) {
+        console.log ("CADASTRO REALIZADO.")
+        console.log("NOMES DOS PARTICIPANTES CADASTRADOS:")
+        console.log (nomeParticipantes);
+        
+            } else {
+    console.log ("CADASTRO NÃO SERA PERMITIDO POR TER EXCEDIDO O LIMITE DE PESSOAS. ")
+    console.log ("FIM"); }
+                
+    
+
+
+
+      
+      
+        
+
+    
+    
+    
+    
